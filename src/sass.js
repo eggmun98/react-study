a43207ec840f4d53b3501545338e350de1dc7e93
eggmun98 <eggmun98@gmail.com>
2023-10-22 16:04:19 +0900

    [2023.10.22] Sass 변수 사용

--- src/sass.js
+++ src/sass.js
@@ -2,10 +2,27 @@ import { Component } from "react";
 import "./assets/sass.scss";
 
 export default class Sass extends Component {
+  state = {
+    isColorChanged: false,
+  };
+
+  toggleColor = () => {
+    this.setState((prevState) => ({
+      isColorChanged: !prevState.isColorChanged,
+    }));
+  };
+
   render() {
     return (
       <div className="SassStyle">
         <h1>Sass</h1>
+        <p>무슨 색일까요?</p>
+        <button
+          onClick={this.toggleColor}
+          className={this.state.isColorChanged ? "a" : ""}
+        >
+          색깔 변경
+        </button>
       </div>
     );
   }
